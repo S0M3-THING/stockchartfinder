0f5fd5d8726732bb0b8cf5cabe21d8788eccb0fe
S0M3-THING <akashs4742@gmail.com>
2025-04-04 09:07:32 -0700

    Back to original

--- app.py
+++ app.py
@@ -99,7 +99,9 @@ def analyze():
         return jsonify({"error": "An error occurred", "details": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=False, host="127.0.0.1", port=5001) 
+    #app.run(debug=False, host="127.0.0.1", port=5001) 
+    app.run(debug=False, host="0.0.0.0", port=5001)
+
 
 '''
 if __name__ == "__main__":
